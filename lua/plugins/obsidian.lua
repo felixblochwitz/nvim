5202cc133a7987fdd17eea891d65673064aac017
felixblochwitz <felix.blochwitz@gmail.com>
2024-08-23 09:15:27 +0200

    ist halt so

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -156,8 +156,8 @@ return {
 			---@param url string
 			follow_url_func = function(url)
 				-- Open the URL in the default web browser.
-				-- vim.fn.jobstart({ "open", url }) -- Mac OS
-				vim.fn.jobstart({"xdg-open", url})  -- linux
+				vim.fn.jobstart({ "open", url }) -- Mac OS
+				-- vim.fn.jobstart({"xdg-open", url})  -- linux
 				-- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
 				-- vim.ui.open(url) -- need Neovim 0.10.0+
 			end,
