2dd1335cf414bb1e45d66f3d7e7a131843dd4258
felixblochwitz <felix.blochwitz@gmail.com>
2024-09-04 01:38:50 +0200

    add color picker

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,4 +1,4 @@
-Active_scheme = "abscs"
+Active_scheme = "vscode"
 -- the lazy module import has to go first
 -- Otherwise some of my config might not work, because for some of the
 -- config, modules need to be loaded.
