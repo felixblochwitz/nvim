f67db109906e8a9007b2268890efafadd97851cc
fblochwitz <felix.blochwitz@gmail.com>
2024-09-09 16:02:17 +0200

    add ruff

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,65 +1,76 @@
 return {
-	-- install mason
-	-- basic install so far -> TODO: see if there are any options I might need
-	{
-		"williamboman/mason.nvim",
-		opts = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
+  -- install mason
+  -- basic install so far -> TODO: see if there are any options I might need
+  {
+    "williamboman/mason.nvim",
+    opts = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
 
-	-- install mason lsp-config (for ensure installed)
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"cssls",
-					"emmet_language_server",
-					"html",
-					"lua_ls",
-					"pyright",
-					"ts_ls",
+  -- install mason lsp-config (for ensure installed)
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "cssls",
+          "emmet_language_server",
+          "html",
+          "lua_ls",
+          "pyright",
+          "ts_ls",
           "clangd",
-				},
-			})
-		end,
-	},
+          "ruff",
+        },
+      })
+    end,
+  },
 
-	-- install mason null-ls (for ensure installed)
-	{
-		"jay-babu/mason-null-ls.nvim",
-		opts = function()
-			require("mason-null-ls").setup({
-				automatic_installation = false,
-				ensure_installed = { "black", "djlint", "isort", "prettier", "stylua", "clang-format" },
-			})
-		end,
-	},
+  -- install mason null-ls (for ensure installed)
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function()
+      require("mason-null-ls").setup({
+        automatic_installation = false,
+        ensure_installed = { "black", "clang-format", "djlint", "isort", "prettier", "stylua" },
+      })
+    end,
+  },
 
-	-- install nvim-lsp
-	-- link to all server configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	-- when I add a language server here, I also need to add it
-	-- to /lua/plugins/completion/init.lua (to the list at the bottom)
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.emmet_language_server.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.html.setup({})
-			lspconfig.lua_ls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.ts_ls.setup({})
+  -- install nvim-lsp
+  -- link to all server configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  -- when I add a language server here, I also need to add it
+  -- to /lua/plugins/completion/init.lua (to the list at the bottom)
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.emmet_language_server.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.ts_ls.setup({})
       lspconfig.clangd.setup({})
-		end,
-	},
+      lspconfig.ruff.setup({
+        init_options = {
+          settings = {
+            organizeImports = false,
+            -- rest of settings is in ~/.config/ruff/pyproject.toml
+            -- can be overridden with a project level pyproject.toml
+            -- I took inspiration from the pandas project for my global pyproject.toml
+          },
+        },
+      })
+    end,
+  },
 }
