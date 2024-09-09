ac95641e15fded2278b7308fae504271c975717f
fblochwitz <felix.blochwitz@gmail.com>
2024-09-09 16:02:05 +0200

    add rename functionality

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,6 @@
 -- recommended setting for tabs from the documentation
 -- https://neovim.io/doc/user/options.html#'tabstop'
--- Setting these to 2 seems to solve my issue with different 
+-- Setting these to 2 seems to solve my issue with different
 -- indentation per file type
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -12,16 +12,18 @@ vim.opt.expandtab = true
 -- TODO: figure out where to best put these
 vim.keymap.set({ "n", "v" }, "gd", vim.lsp.buf.definition)
 vim.keymap.set({ "n", "v" }, "gr", vim.lsp.buf.references)
-vim.keymap.set({"n", "v"}, "gI", vim.lsp.buf.implementation)
-vim.keymap.set({"n", "v"}, "gY", vim.lsp.buf.type_definition)
-vim.keymap.set({"n", "v"}, "gD", vim.lsp.buf.declaration)
-vim.keymap.set({"n", "v"}, "K", vim.lsp.buf.hover)
-vim.keymap.set({"n", "v"}, "gK", vim.lsp.buf.signature_help)
-vim.keymap.set({"i"}, "<c-k>", vim.lsp.buf.signature_help)
-vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action)
-vim.keymap.set({"n", "v"}, "<leader>cc", vim.lsp.codelens.run)
-vim.keymap.set({"n", "v"}, "<leader>cC", vim.lsp.codelens.refresh)
+vim.keymap.set({ "n", "v" }, "gI", vim.lsp.buf.implementation)
+vim.keymap.set({ "n", "v" }, "gY", vim.lsp.buf.type_definition)
+vim.keymap.set({ "n", "v" }, "gD", vim.lsp.buf.declaration)
+vim.keymap.set({ "n", "v" }, "K", vim.lsp.buf.hover)
+vim.keymap.set({ "n", "v" }, "gK", vim.lsp.buf.signature_help)
+vim.keymap.set({ "i" }, "<c-k>", vim.lsp.buf.signature_help)
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set({ "n", "v" }, "<leader>cc", vim.lsp.codelens.run)
+vim.keymap.set({ "n", "v" }, "<leader>cC", vim.lsp.codelens.refresh)
 vim.keymap.set({ "n" }, "<leader>cf", vim.lsp.buf.format)
+vim.keymap.set({ "n" }, "<leader>cr", vim.lsp.buf.rename)
+
 -- the keymaps below are from LazyVim, don't know if I'll need them, but I don't want to delete yet
 -- vim.keymap.set({"n", "v"}, "<leader>cr", vim.lsp.codelens.rename)
 -- { "<leader>cR", LazyVim.lsp.rename_file, desc = "Rename File", mode ={"n"}, has = { "workspace/didRenameFiles", "workspace/willRenameFiles" } }, TODO: checkout if I can imlement this
@@ -57,7 +59,7 @@ vim.opt.conceallevel = 1
 
 -- map scrolling to <c-j> and <c-k> to be able to scroll past end of file
 -- scroll down
-vim.keymap.set({"n"}, "<c-n>", "<c-e>")
+vim.keymap.set({ "n" }, "<c-n>", "<c-e>")
 -- scroll up
-vim.keymap.set({"n"}, "<c-m>", "<c-y>")
+vim.keymap.set({ "n" }, "<c-m>", "<c-y>")
 -- changed it, because it was interfering with my tmux config
