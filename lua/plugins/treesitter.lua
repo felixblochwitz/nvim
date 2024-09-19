b7efeaab1caf088f403f571c2fdd6ec04331f911
fblochwitz <felix.blochwitz@gmail.com>
2024-09-19 10:07:55 +0200

    disable html indent in tressitter. remove rust from ensure installed, add c

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		opts = function()
-			require("nvim-treesitter.configs").setup({
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = { enable = true },
-				ensure_installed = {
-					"python",
-					"javascript",
-					"lua",
-					"luadoc",
-					"luap",
-					"css",
-					"html",
-					"scala",
-					"rust",
-					"markdown",
-					"markdown_inline",
-				},
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true, disable = { "html" } },
+        ensure_installed = {
+          "python",
+          "javascript",
+          "lua",
+          "luadoc",
+          "luap",
+          "c",
+          "css",
+          "html",
+          "scala",
+          "markdown",
+          "markdown_inline",
+        },
+      })
+    end,
+  },
 }
