d007467b67944dd834260e608be99d5d94ee2197
fblochwitz <felix.blochwitz@gmail.com>
2024-09-24 17:34:06 +0200

    minor: add rose-pine colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,46 +1,47 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		opts = { style = "moon" },
-	},
-	{
-		"Everblush/nvim",
-		name = "everblush",
-	},
-	{
-		"akinsho/horizon.nvim",
-		version = "*",
-		name = "horion",
-	},
-	{
-		"Abstract-IDE/Abstract-cs",
-	},
-	{
-		"rebelot/kanagawa.nvim",
-	},
-	{
-		"mellow-theme/mellow.nvim",
-	},
-	{
-		"killitar/obscure.nvim",
-	},
-	{
-		"NTBBloodbath/doom-one.nvim",
-	},
-	{
-		"Tsuzat/NeoSolarized.nvim",
-		opts = {
-			transparent = false,
-		},
-		name = "neosolarized",
-	},
-	{
-		"savq/melange-nvim",
-	},
-	{
-		"Mofiqul/vscode.nvim",
-	},
-	{
-		"nyoom-engineering/oxocarbon.nvim",
-	},
+  {
+    "folke/tokyonight.nvim",
+    opts = { style = "moon" },
+  },
+  {
+    "Everblush/nvim",
+    name = "everblush",
+  },
+  {
+    "akinsho/horizon.nvim",
+    version = "*",
+    name = "horion",
+  },
+  {
+    "Abstract-IDE/Abstract-cs",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "mellow-theme/mellow.nvim",
+  },
+  {
+    "killitar/obscure.nvim",
+  },
+  {
+    "NTBBloodbath/doom-one.nvim",
+  },
+  {
+    "Tsuzat/NeoSolarized.nvim",
+    opts = {
+      transparent = false,
+    },
+    name = "neosolarized",
+  },
+  {
+    "savq/melange-nvim",
+  },
+  {
+    "Mofiqul/vscode.nvim",
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+  },
+  { "rose-pine/neovim", name = "rose-pine" },
 }
