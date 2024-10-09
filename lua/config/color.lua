2998efd1de2ef33364e9f63f819e7b8b407e88ae
felixblochwitz <felix.blochwitz@gmail.com>
2024-10-09 02:31:14 +0200

    add idea for how to improve color picker

--- lua/config/color.lua
+++ lua/config/color.lua
@@ -1,3 +1,15 @@
+--[[
+-- TODO: think about implementing a color scheme background table for the colorschemes
+--       for the color schemes that do not provide separate dark and light themes by default
+--
+-- This is a suggestion from ChatGPT on how to implement this:
+-- local scheme_backgrounds = {
+--    vscode = "dark",             -- Valid identifier
+--    ["vscode-light"] = "light",  -- Key with a hyphen
+--    tokyonight = "dark",         -- Valid identifier
+--    ["tokyonight-day"] = "light" -- Key with a hyphen
+--  }
+--]]
 local function switch_scheme(theme)
   local scheme_name = theme.args
   vim.cmd("colorscheme " .. scheme_name)
